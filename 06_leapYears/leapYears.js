3d3e2368d3f6f12b2aeb6438e475428b9d223e4a
Mathieu Saintes <mathieu.saintes@gmail.com>
2023-11-21 16:51:10 +0800

    Rewrite the leapYears function
    Writing the decision tree shows that only two scenarios lead to
    returning leap years:
    - Divisible by 4, 100 and 400. But as divisibility by 400 implies
    divisibility by 4 and 100, testing the divisibility by 400 is enough
    - Divisible by 4 but not by 100
    
    Hence the one line condition.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,9 +2,10 @@
 // However, years divisible by 100 are not leap years (such as 1800 and 1900) 
 // unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years)
 const leapYears = function(year) {
-  if (year % 4 !== 0) return false;
-  if (year % 100 !== 0) return true;
-  if (year % 400 == 0) return true;
+  const isDivisibleBy4 = year % 4 === 0;
+  const isDivisibleBy100 = year % 100 === 0;
+  const isDivisibleBy400 = year % 400 === 0;
+  if (isDivisibleBy400 || isDivisibleBy4 && !isDivisibleBy100) return true;
   return false;
 };
 
