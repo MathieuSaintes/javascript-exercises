2d7f087b02ec3fbaf2331d53d12ec4a5ac8611e5
Mathieu Saintes <mathieu.saintes@gmail.com>
2023-11-21 15:36:43 +0800

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+  const values = Array.prototype.slice.call(arguments, 1);
+  return array.filter(element => {
+    for (const value of values) {
+        if (element === value) return false;
+    }
+    return true;
+  });
 };
 
 // Do not edit below this line
