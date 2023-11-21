61fd750cf27be87df28e77c15217b39e1810104d
Mathieu Saintes <mathieu.saintes@gmail.com>
2023-11-21 17:20:30 +0800

    Solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Tf) {
+  return +( (5/9) * (Tf - 32) ).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Tc) {
+  return +(Tc * 9 / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
