9e7081cee3b4459bdb48fa15a1552598cb57835b
Mathieu Saintes <mathieu.saintes@gmail.com>
2023-11-21 16:25:56 +0800

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  if (
+    a < 0 ||
+    b < 0 ||
+    typeof a !== 'number' ||
+    typeof b !== 'number'
+    ) return 'ERROR';
+  if (a > b) return sumAll(b,a);  
+  let total = 0;
+  for (let i = a; i <= b; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
