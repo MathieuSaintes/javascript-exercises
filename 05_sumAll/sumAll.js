1c861b0625ce3fb1a198ff66b1401a5bf8c8a1e5
Mathieu Saintes <mathieu.saintes@gmail.com>
2024-06-13 12:34:36 +0200

    Solve again exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(...pair) {
 
+    for (const item of pair) {
+        if (typeof item != 'number' || item < 0) return 'ERROR';
+    }
+
+    const [smallest, largest] = pair.sort();
+
+    let sum = 0;
+    for (let i = smallest; i <= largest; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
