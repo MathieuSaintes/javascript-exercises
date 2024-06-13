6e0863df32c0b1ad57cdecf589d4a9a093f36a17
Mathieu Saintes <mathieu.saintes@gmail.com>
2024-06-13 10:56:32 +0200

    Provide an alternate solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,18 @@
-const reverseString = function(string) {
+const reverseString = function(string){
+    const array = string.split('');
+    array.reverse();
+    const reversedString = array.join('');
+    return reversedString;
+}
+
+/* const reverseString = function(string) {
     const l = string.length;
     let output = '';
     for (i = 1; i <= l; i++) {
         output += string[l-i];
     }
     return output;
-};
+}; */
 
 // Do not edit below this line
 module.exports = reverseString;
