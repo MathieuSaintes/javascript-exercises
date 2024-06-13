445c6221171b2f52605effe058dcd91c160340af
Mathieu Saintes <mathieu.saintes@gmail.com>
2024-06-13 10:39:44 +0200

    Solve again exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const l = string.length;
+    let output = '';
+    for (i = 1; i <= l; i++) {
+        output += string[l-i];
+    }
+    return output;
 };
 
 // Do not edit below this line
