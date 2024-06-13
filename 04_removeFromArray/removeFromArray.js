c6f050b79286da7fe3fcc341303d3ff30187b18d
Mathieu Saintes <mathieu.saintes@gmail.com>
2024-06-13 11:51:19 +0200

    Provide a more concise solution to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function(array, ...values) {
+const removeFromArray = function(array, ...valuesToBeRemoved) {
+    return array.filter(value => !valuesToBeRemoved.includes(value));
+}
+
+/* const removeFromArray = function(array, ...values) {
     for (let i = 0; i < array.length;) {
         if (values.includes(array[i])) {
             array.splice(i, 1);
@@ -7,7 +11,7 @@ const removeFromArray = function(array, ...values) {
         }
     }
     return array;
-};
+}; */
 
 // Do not edit below this line
 module.exports = removeFromArray;
