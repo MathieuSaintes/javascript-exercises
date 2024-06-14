66d9516a3d108a6e77e1acb7f7f59c5580f6a225
Mathieu Saintes <mathieu.saintes@gmail.com>
2024-06-14 14:10:07 +0200

    Solve again exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function roundTo1Decimal(float) {
+  return +float.toFixed(1);
+}
+
+const convertToCelsius = function(Tf) {
+  const Tc = (Tf-32)*(5/9);
+  return roundTo1Decimal(Tc);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Tc) {
+  const Tf = (9/5)*Tc+32;
+  return roundTo1Decimal(Tf);
 };
 
 // Do not edit below this line
